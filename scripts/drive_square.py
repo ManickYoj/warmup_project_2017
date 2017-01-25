f72085bd7a8a6d05f1df698923d11cd763bdc439
Nick Francisci <nickfrancisci@gmail.com>
2017-01-25 15:22:16 -0500

    Completed drive square behavior

--- scripts/drive_square.py
+++ scripts/drive_square.py
@@ -1,9 +1,10 @@
-
 #!/usr/bin/env python
 
 import rospy
 from geometry_msgs.msg import Twist, Vector3
 from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+import math
 
 DIRECTIONS = {
   'FORWARD': Twist(linear=Vector3(1, 0, 0), angular=Vector3(0, 0, 0)),
@@ -13,52 +14,123 @@ DIRECTIONS = {
   'STOP': Twist(linear=Vector3(0, 0, 0), angular=Vector3(0, 0, 0)),
 }
 
+def poseToXYTheta(pose):
+  """
+  Converts the transform and rotation data from a pose into an
+  (x, y, theta) tuple. Fully derived from Paul Ruvolo's
+  2015 comprobo libraries.
+  """
+
+  orientation = (
+    pose.orientation.x,
+    pose.orientation.y,
+    pose.orientation.z,
+    pose.orientation.w
+  )
+
+  return (
+    pose.position.x,
+    pose.position.y,
+    euler_from_quaternion(orientation)[2]
+  )
+
+def wrapPi(val):
+  """
+  Wraps the input value so that it is always within
+  the range [-pi, pi]. Derived from Paul Ruvolo's 2015
+  comprobo libraries.
+  """
+
+  return math.atan2(math.sin(val), math.cos(val))
+
+def diffAngle(a, b):
+  """
+  Calculates the difference between angle a and angle b (both should be in radians)
+  the difference is always based on the closest rotation from angle a to angle b.
+  Derived from Paul Ruvolo's 2015 comprobo libraries
+  """
+  a = wrapPi(a)
+  b = wrapPi(b)
+  d1 = a-b
+  d2 = 2*math.pi - math.fabs(d1)
+  if d1 > 0:
+    d2 *= -1.0
+  return d1 if math.fabs(d1) < math.fabs(d2) else d2
+
 class DriveSquare:
-  def __init__(self):
-    rospy.init_node('teleop')
+  """
+  This class contains the behavior for driving in a square. On initialization,
+  it creates a ros node with a publisher and subscriber and begins to attempt
+  to drive either straight or turn. It hijacks the cmd_vel topic, so it will
+  interfere with standard teleoperation.
+  """
+
+  def __init__(self, debug=False):
+    rospy.init_node('drive_square')
     rospy.Subscriber('/odom', Odometry, self.dispatcher)
     self.publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-    
-    self.state = "driving"
 
-    r = rospy.rate(10)
+    # Potential states of robot
+    self.states = {
+      "debug": self.debug,
+      "driving": self.driving,
+      "turning": self.turning
+    }
+
+    # Set initial state
+    self.state = self.states["driving"] if not debug else self.state["debug"]
+    self.start_x = None
+    self.start_y = None
+    self.start_angle = None
+
+    r = rospy.Rate(10)
     while not rospy.is_shutdown():
       r.sleep()
-      
-  def dispatcher(odom):
-    if self.state == "driving":
-      self.check_odom(odom)
-    else:
-      self.spin(odom)
 
-  def check_odom(odom):
-    pose = odom.pose.pose
-    cur_x = pose.position.x
-    cur_y = pose.position.y
-    
-    if not self.start_pos:
-      self.start_pos = (pose.position.x, pose.position.y)
-      
-    start_x, start_y = self.start_pos
-    
-		if sqrt((cur_x - start_x)^2+ (cur_y - start_y)^2) >= 1:
-      self.state = turning
-      dispatcher(odom)
-      
-  	
-    self.prev_x = cur_x
-    self.prev_y = cur_y
-    
-  def spin(odom):
-    pose = odom.pose.pose
-    self.start_pos = (pose.position.x, pose.position.y)
-    
-    orientation = tf.transformations.euler_from_quaternion(pose.orientation)
-  	if not self.target_angle:
-      self.target_angle = # TODO
-    
-    # TODO: Set target angle (and account for angle wrapping) and switch states if achieved
-    # Otherwise, publish turn command
+  def dispatcher(self, odom):
+    self.state(poseToXYTheta(odom.pose.pose))
+
+  def debug(self, odom):
+    print odom
+
+  def switchBehavior(self):
+    self.start_x = None
+    self.start_y = None
+    self.start_angle = None
+
+    if self.state == self.states["driving"]:
+      self.state = self.states["turning"]
+    else:
+      self.state = self.states["driving"]
+
+  def driving(self, odom, sidelength=.5):
+    x, y, _ = odom
+
+    if self.start_x is not None and self.start_y is not None :
+      dis = math.sqrt((x - self.start_x)**2 + (y - self.start_y)**2)
+      if dis < sidelength:
+        self.publisher.publish(DIRECTIONS["FORWARD"])
+      else:
+        self.switchBehavior()
+    else:
+      self.start_x = x
+      self.start_y = y
+
+
+  def turning(self, odom, turn_angle = math.pi/2, error_margin=0.5):
+    _, _, angle = odom
+
+    # Note: the error margin accounts for the fact that the odometry reading
+    # is only updated once per four publications, and on top of that, seems to
+    # undershoot the actual turn distance. The error margin was calibrated
+    # to form a rough square in testing.
+    if self.start_angle is not None:
+      if diffAngle(angle, self.start_angle) < (turn_angle-error_margin):
+        self.publisher.publish(DIRECTIONS["LEFT"])
+      else:
+        self.switchBehavior()
+    else:
+      self.start_angle = angle
 
 if __name__ == "__main__":
   d = DriveSquare()
